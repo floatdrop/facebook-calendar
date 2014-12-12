613b99674a222e588c044d14e5df89bfcdb70fd1
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-13 02:40:22 +0500

    :tropical_drink: add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
+
+gulp.task('connect', function () {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('watch', function () {
+    watch(['!jspm_packages', 'index.*', 'lib/**/*'])
+        .pipe(connect.reload());
+});
+
+gulp.task('default', ['connect', 'watch']);
